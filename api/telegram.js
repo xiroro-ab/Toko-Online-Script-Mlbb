ea2e96d2031b80a8260cc2b412bd10649bdbcf5f
xiroro-ab <aris.bermansyah14@gmail.com>
2026-08-30 10:43:19 +0700

    Update Apps Script URL to new deployment

--- api/telegram.js
+++ api/telegram.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   try {
     const raw = typeof req.body === "string" ? req.body : JSON.stringify(req.body || {});
-    await fetch("https://script.google.com/macros/s/AKfycbyfBzWmShFfm00QdleoSpxhgwPeRmwhu-OSte6JZcuza1G9sIfmlXgdeSjV1BLgsqoj/exec", {
+    await fetch("https://script.google.com/macros/s/AKfycbyPty75oBpOL07t9E6PYcNAm43Zu52Ul9DLnnOuC8YS41s8MdfIIKg2xvMYhJm9eSly/exec", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: raw
